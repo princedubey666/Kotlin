8c9f4686e1ce80adcd7ccaa57cd8aa7e52f2d086
Prince Dwivedi <123713016+princedubey666@users.noreply.github.com>
2026-01-06 11:50:07 +0530

    Add Kotlin program to demonstrate variable usage

---
+++ Basics/values.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val isRaining = true
+    println(!isRaining)
+
+    val a:Int = 22
+    println(a)
+
+    val b:Float = 1.587
+    println(b)
+
+    val c:String = "Prince"
+    println(c)
+
+}
